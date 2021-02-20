438f51dfe192024650b69331ff5afad700bba530
Bishop98 <joshjwisser98@gmail.com>
2021-02-20 01:59:35 -0700

    progress

--- src/App.js
+++ src/App.js
@@ -1,14 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import Cell from './components/cell.js';
 import './App.css';
 
-const gridSize = 100;
+//Maffah mah'iks
 
 function App() {
+  const [gridSize, setGridSize] = useState(10);
+  const [cellSize, setCellSize] = useState(50);
+  const [selectedTool, setSelectedTool] = useState('start')
+
+  let count = 0;
+  
+
+  const matrix = Array.from(Array(gridSize), () => {
+    try {
+      return new Array(gridSize).fill(<Cell onClick={selectedTool} bigness={cellSize}/>);
+    } finally {
+      console.log(count);
+      count++;
+    };
+  });
+  // const matrixData = matrix.map((row, rowIndex) => {
+  //   return row.map((node,nodeIndex)=>{node.key = rowIndex*gridSize+nodeIndex})
+  // })
+  // const matrix = new Array(gridSize).fill(null).map((currentElement, elementIndex) => {
+  //   try {
+  //     return new Array(gridSize).fill(<Cell onClick={selectedTool} bigness={cellSize}/>);
+  //   } finally {
+  //     console.log(count);
+  //     count++;
+  //   }
+  // });
+  const [grid, setGrid] = useState(matrix);
+
+  const gridBoundries = {
+    minWidth: (matrix.length)*cellSize,
+    maxWidth:(matrix.length)*cellSize,
+  }
   return (
-    <div className="App">
-      <Cell kissme="nothanks"/>
+    <div style={gridBoundries} className="matrix-container"> 
+      {console.log(grid)}
+      <h1>A* Pathfinding Demo</h1>
+      {matrix}
+      {/* {grid} */}
+      <button onClick={() => setSelectedTool('start')}>Start</button>
+      <button onClick={() => setSelectedTool('finish')}>Finish</button>
+      <button onClick={() => setSelectedTool('wall')}>Wall</button>
+      
     </div>
   );
 }
--- src/components/cell.js
+++ src/components/cell.js
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Cell = (props) => {
-    return <p>{props.kissme}</p>
+    const [gridPos, setGridPos] = useState();
+    const [color, setColor] = useState('white');
+    const mystyle = {
+        backgroundColor: color,
+        border: "1px solid black",
+        margin:0,
+        marginBottom: -4,
+        padding: Number((props.bigness/2)-1),
+        width: props.bigness,
+        boxSizing: "border-box",
+        display: "inline-block"
+      };
+    const updateColor = () => {
+      if (props.currentTool === 'start') {
+        setColor('green');
+      } else if (props.currentTool === 'finish') {
+        setColor('red');
+      } else if (props.currentTool === 'wall') {
+        setColor('black');
+      }
+    };
+    console.log(props.poopy);
+    return <div style={mystyle} onClick={() => {console.log(props.onClick)}}></div>
 }
 
 export default Cell;
