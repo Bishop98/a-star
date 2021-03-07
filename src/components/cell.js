19cb20ffed01f3d76f985abceb9c240c0d039f6a
Bishop98 <joshJwisser98@gmail.com>
2021-03-07 12:04:17 -0700

    Update cell.js

--- src/components/cell.js
+++ src/components/cell.js
@@ -1,29 +1,27 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 const Cell = (props) => {
-    const [gridPos, setGridPos] = useState();
-    const [color, setColor] = useState('white');
-    const mystyle = {
-        backgroundColor: color,
-        border: "1px solid black",
-        margin:0,
-        marginBottom: -4,
-        padding: Number((props.bigness/2)-1),
-        width: props.bigness,
-        boxSizing: "border-box",
-        display: "inline-block"
-      };
-    const updateColor = () => {
-      if (props.currentTool === 'start') {
-        setColor('green');
-      } else if (props.currentTool === 'finish') {
-        setColor('red');
-      } else if (props.currentTool === 'wall') {
-        setColor('black');
-      }
-    };
-    console.log(props.poopy);
-    return <div style={mystyle} onClick={() => {console.log(props.onClick)}}></div>
+  const decideColor = () => {
+    const stateColorCorrelation = {
+      "w":"white",
+      "W":"black",
+      "f":"red",
+      "s":"green",
+      "S":"cyan"
+    }
+    return stateColorCorrelation[props.appState.FEN[props.nodeIndex]]
+  }
+  const cellStyle = {
+    backgroundColor: decideColor(),
+    border: "1px solid black",
+    margin:0,
+    marginBottom: -4,
+    padding: Number((props.appState.cellSize/2)-1),
+    width: props.appState.cellSize,
+    boxSizing: "border-box",
+    display: "inline-block"
+  };
+  return <div style={cellStyle} onMouseOver={()=>props.mouseover({index:props.nodeIndex})} onClick={()=>props.clicked({index:props.nodeIndex})}></div>
 }
 
 export default Cell;
