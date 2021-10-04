1c5cb46b296cda47341b07fa91c98bebdf2eb0c2
Bishop98 <joshJwisser98@gmail.com>
2021-10-03 22:07:12 -0600

    Update flatten.js
    fixed code styling

--- src/functions/flatten.js
+++ src/functions/flatten.js
@@ -1,14 +1,14 @@
 const flatten = function(arr, result = []) {
-    for (let i = 0, length = arr.length; i < length; i++) {
-      const value = arr[i];
-      if (Array.isArray(value)) {
-        flatten(value, result);
-      } else {
-        result.push(value);
-      }
+  for (let i = 0, length = arr.length; i < length; i++) {
+    const value = arr[i];
+    if (Array.isArray(value)) {
+      flatten(value, result);
+    } else {
+      result.push(value);
     }
-    return result;
-  };
+  }
+  return result;
+};
 
 
 export default flatten;
